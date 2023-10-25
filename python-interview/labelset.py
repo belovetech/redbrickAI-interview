7be4b730b90147dd25d275c624f7745720924446
belovetech <belovetech@gmail.com>
2023-10-25 05:51:49 +0100

    (chore): improve the labelset class

--- python-interview/labelset.py
+++ python-interview/labelset.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 """Labelset class."""
+import json
 
 class Case:
     """A case is a set of events."""
     def __init__(self, case_id: str):
         self.dp_id = case_id
         self.events = []
-        self.state_by_branch = State
+        self.state_by_branch = {}
 
     def __repr__(self):
         return f"Case({self.dp_id}, {self.events})"
 
     def __str__(self):
         return str({"dp_id": self.dp_id, "events": self.events})
+
+
+class Event:
+    """An event is a tuple of (user, td_id, action)."""
+    def __init__(self, user: str, td_id: str, action: str):
+        self.user = user
+        self.td_id = td_id
+        self.action = action
+
+    def __repr__(self):
+        return f"Event({self.user}, {self.td_id}, {self.action})"
+
+    def __str__(self):
+        return str({"user": self.user, "td_id": self.td_id, "action": self.action})
 
 
 class State:
@@ -47,11 +62,13 @@ class Labelset:
         """Get a case."""
         return self.cases.get(dp_id, None)
 
-    def annotate_case(self, dp_id:str, user:str, td:str):
+    def annotate_case(self, dp_id:str, user:str, td:str) -> None:
         """Annotate a case."""
         case = self.get_case(dp_id)
-        state = case.state_by_branch.get(user, None)
+        if not case:
+            raise ValueError(f"Case with dp_id {dp_id} does not exist")
 
+        state = case.state_by_branch.get(user, None)
         if not state:
             case.state_by_branch[user] = State(td)
         else:
@@ -59,23 +76,22 @@ class Labelset:
                 raise ValueError(f"Case with dp_id {dp_id} already submitted")
             case.state_by_branch[user] = State(td)
 
-        if not case:
-            raise ValueError(f"Case with dp_id {dp_id} does not exist")
-        case.events.append((user, td))
+        case.events.append(Event(user, td, "annotate"))
 
-    def sign_off_on_case(self, dp_id:str, user:str, td:str):
+    def sign_off_on_case(self, dp_id:str, user:str, td:str) -> None:
         """Sign off on a case."""
         case = self.get_case(dp_id)
-        case.state_by_branch[user].is_submitted = True
         if not case:
             raise ValueError(f"Case with dp_id {dp_id} does not exist")
-        case.events.append((user, td))
+
+        case.state_by_branch[user].is_submitted = True
+
+        case.events.append(Event(user, td, "sign_off"))
 
 
-    def review_passed(self, dp_id:str, user:str, td:str):
+    def review_passed(self, dp_id:str, user:str, td:str) -> None:
         """Case Review passed."""
         case = self.get_case(dp_id)
-
         if not case:
             raise ValueError(f"Case with dp_id {dp_id} does not exist")
 
@@ -84,9 +100,9 @@ class Labelset:
         else:
             raise ValueError(f"Case with dp_id {dp_id} not submitted")
 
-        case.events.append((user, td))
+        case.events.append(Event(user, td, "review_passed"))
 
-    def review_failed(self, dp_id:str, user:str, td:str):
+    def review_failed(self, dp_id:str, user:str, td:str) -> None:
         """Case Review failed."""
         case = self.get_case(dp_id)
 
@@ -99,9 +115,9 @@ class Labelset:
         else:
             raise ValueError(f"Case with dp_id {dp_id} not submitted")
 
-        case.events.append((user, td))
+        case.events.append(Event(user, td, "review_failed"))
 
-    def merge_branches(self, dp_id:str, users:str, merged_branch:str, td:str):
+    def merge_branches(self, dp_id:str, users:str, merged_branch:str, td:str)-> None:
         """Merge branches."""
         case = self.get_case(dp_id)
         if not case:
@@ -115,24 +131,52 @@ class Labelset:
             if case.state_by_branch[user].is_submitted is False:
                 case.state_by_branch[merged_branch].is_submitted = False
 
-        case.events.append((merged_branch, td))
+        case.events.append(Event(merged_branch, td, "merge_branches"))
+
+
+    def get_cases(self):
+        _dict = {}
+        for case in self.cases:
+            _dict[case] = {}
+            _dict[case]["dp_id"] = self.cases[case].dp_id
+            _dict[case]["events"] = []
+            _dict[case]["state_by_branch"] = {}
+            for event in self.cases[case].events:
+                _dict[case]["events"].append(event.__dict__)
+            for branch in self.cases[case].state_by_branch:
+                _dict[case]["state_by_branch"][branch] = self.cases[case].state_by_branch[branch].__dict__
+        return json.dumps(_dict, indent=2)
+
+    def __repr__(self):
+        return f"Labelset({self.cases})"
+
+    def __str__(self):
+        return str({"cases": self.cases})
 
 
 
 
 
 if __name__ == "__main__":
-    case = Case("dp_1")
-    print(case)
 
-    state = State("td_1")
-    print(state)
-
-
+    dp_id = "test1"
     labelset = Labelset()
-    labelset.create_case("dp_1")
-    labelset.annotate_case("dp_1", "user_1", "td_1")
-    # labelset.annotate_case("dp_1", "user_2", "td_2")
-    case = labelset.get_case("dp_1")
-    print(case)
+    labelset.create_case(dp_id)
+    labelset.annotate_case(dp_id, "user1", "td11")
+    labelset.annotate_case(dp_id, "user2", "td21")
+
+    labelset.sign_off_on_case(dp_id, "user1", "td11")
+    labelset.sign_off_on_case(dp_id, "user2", "td21")
+
+    labelset.review_passed(dp_id, "user1", "td11")
+    labelset.review_failed(dp_id, "user2", "td21")
+
+    labelset.merge_branches(dp_id, ["user1", "user2"], "merged_branch", "tdMerged")
+
+    cases = labelset.get_cases()
+    print(cases)
+
+    cases_json = json.loads(cases)
+    print(cases_json)
+
 
